88d0b42d333007b7e2f8238c7e8288556f519caf
QubicGen11 <qubicgen@gmail.com>
2024-07-08 12:49:12 +0530

    This is my first one

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+<div className='flex  justify-center items-center'>
+    <h1 className='text-black font-bold text-3xl'>This is a test</h1>
+
+</div>
+    
+    </>
+  )
+}
+
+export default App
