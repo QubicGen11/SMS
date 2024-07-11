d0c6715d9ebd250b4a6dea6492a077e26cc8d8ad
QubicGen11 <qubicgen@gmail.com>
2024-07-11 16:09:48 +0530

    Added the landing page

--- src/Components/Dashboard_Components/Dashboard.jsx
+++ src/Components/Dashboard_Components/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
 
-      <Sidemenu sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />      {/* Main content */}
+      <Sidemenu sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />      
       <div className="flex flex-col flex-1 w-full overflow-hidden">
         {/* Header */}
 
@@ -27,9 +27,9 @@ const Dashboard = () => {
             <h1 className="font-light text-xs">Have a nice day</h1>
           </div>
 
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <Dbmain />
-          </div>
+     <div className="grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  <Dbmain/>
+</div>
         </main>
       </div>
     </div>
--- src/Components/Dashboard_Components/Dbmain.jsx
+++ src/Components/Dashboard_Components/Dbmain.jsx
@@ -10,59 +10,59 @@ const Dbmain = () => {
  
  <Link
               to="/students"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-3xl w-10/12 shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611744/Students_fxzltp.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Students</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Students</span>
             </Link>
             <Link
               to="/teachers"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white  border border-gray-200 w-10/12  rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611738/Teachers_h8i7rx.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Teachers</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Teachers</span>
             </Link>
             <Link
               to="/attendance"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12  rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611726/Attendance_d9wmem.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Attendance</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Attendance</span>
             </Link>
             <Link
               to="/marks"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12  rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611710/Marks_nf2g0o.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Marks</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Marks</span>
             </Link>
             <Link
               to="/staff"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200  w-10/12 rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611678/management_stv5su.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Staff</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Staff</span>
             </Link>
             <Link
               to="/leaves"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12  rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611672/Leaves_ovcell.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Leaves</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Leaves</span>
             </Link>
             <Link
               to="/announcements"
-              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+              className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12  rounded-3xl shadow-md hover:shadow-xl"
             >
               <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720611657/Announcements_n9o9ed.png" className='w-24' alt="" />
-              <span className="mt-2 text-lg font-semibold">Announcements</span>
+              <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Announcements</span>
             </Link>
             <Link
                 to="/settings"
-                className="flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl"
+                className="flex flex-col items-center justify-center p-4 bg-white border w-10/12   border-gray-200 rounded-3xl shadow-md hover:shadow-xl"
               >
                 <FaCog className="text-4xl text-gray-500" />
-                <span className="mt-2 text-lg font-semibold">Settings</span>
+                <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg  font-semibold">Settings</span>
               </Link>
     </>
   )
