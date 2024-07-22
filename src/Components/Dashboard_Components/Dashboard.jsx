73b35df00fa8b72ab4077ad4f7029625c047d479
QubicGen11 <qubicgen@gmail.com>
2024-07-22 20:25:01 +0530

    Modified the animation

--- src/Components/Dashboard_Components/Dashboard.jsx
+++ src/Components/Dashboard_Components/Dashboard.jsx
@@ -71,7 +71,7 @@ const Dashboard = () => {
             <h1 className="font-light text-xs">Have a nice day</h1>
           </div>
           <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" ref={gridRef}>
-              <Dbmain ref={dbMainRef} />
+              <Dbmain />
             <div >
             </div>
           </div>
--- src/Components/Dashboard_Components/Dbmain.jsx
+++ src/Components/Dashboard_Components/Dbmain.jsx
@@ -1,131 +1,60 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { gsap } from 'gsap';
 
 const Dbmain = () => {
   const linksRef = useRef([]);
 
-  useEffect(() => {
-    linksRef.current.forEach((link, index) => {
-      if (link) {
-        const img = link.querySelector('img');
-        const tl = gsap.timeline({ paused: true });
-
-        tl.to(img, {
-          y: -15,
-          duration: 0.2,
-          ease: "power1.inOut",
-          yoyo: true,
-          repeat: 1
-        });
-
-        link.addEventListener('mouseenter', () => tl.play());
-        link.addEventListener('mouseleave', () => tl.reverse());
-      }
+  const handleMouseEnter = (index) => {
+    const img = linksRef.current[index].querySelector('img');
+    gsap.to(img, {
+      y: -15,
+      duration: 0.2,
+      ease: "power1.inOut",
+      yoyo: true,
+      repeat: 1
     });
-  }, []);
+  };
+
+  const handleMouseLeave = (index) => {
+    const img = linksRef.current[index].querySelector('img');
+    gsap.to(img, {
+      y: 0,
+      duration: 0.2,
+      ease: "power1.inOut"
+    });
+  };
 
   return (
     <>
-      <Link
-        to="/students"
-        className="flex flex-col items-center justify-center p-4 transition-transform duration-300 ease-in-out transform hover:scale-105 bg-white border border-gray-200 rounded-3xl w-10/12 shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[0] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772446/Metrics_rretv9.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Metrics</span>
-      </Link>
-      <Link
-        to="/enrolements"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[1] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772427/Registration_mmddw4.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Enrollment/Registration</span>
-      </Link>
-      <Link
-        to="/attendance"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[2] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772415/Finances_gfoyxs.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Finances</span>
-      </Link>
-      <Link
-        to="/marks"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[3] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772394/User_Management_m0qsvy.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">User Management</span>
-      </Link>
-      <Link
-        to="/students"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 rounded-3xl w-10/12 shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[4] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772378/Students_quljje.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Students</span>
-      </Link>
-      <Link
-        to="/teachers"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[5] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772365/Teachers_wdoq9f.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Teachers</span>
-      </Link>
-      <Link
-        to="/attendance"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[6] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772346/Attendance_l53an9.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Attendance</span>
-      </Link>
-      <Link
-        to="/marks"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[7] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772326/Marks_ynnahe.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Marks</span>
-      </Link>
-      <Link
-        to="/staff"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[8] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772308/Staff_ukgko1.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Staff</span>
-      </Link>
-      <Link
-        to="/leaves"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[9] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772269/Leaves_qno92t.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Leaves</span>
-      </Link>
-      <Link
-        to="/announcements"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[10] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772198/Announcements_d7rrb4.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Announcements</span>
-      </Link>
-      <Link
-        to="/announcements"
-        className="transition-transform duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center justify-center p-4 bg-white border border-gray-200 w-10/12 rounded-3xl shadow-md hover:shadow-xl"
-        ref={(el) => linksRef.current[11] = el}
-      >
-        <img src="https://res.cloudinary.com/devewerw3/image/upload/v1720772183/Settings_boymis.png" className='w-24' alt="" />
-        <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">Settings</span>
-      </Link>
-
+      {[
+        { to: "/students", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772446/Metrics_rretv9.png", text: "Metrics" },
+        { to: "/enrolements", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772427/Registration_mmddw4.png", text: "Enrollment/Registration" },
+        { to: "/attendance", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772415/Finances_gfoyxs.png", text: "Finances" },
+        { to: "/marks", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772394/User_Management_m0qsvy.png", text: "User Management" },
+        { to: "/students", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772378/Students_quljje.png", text: "Students" },
+        { to: "/teachers", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772365/Teachers_wdoq9f.png", text: "Teachers" },
+        { to: "/attendance", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772346/Attendance_l53an9.png", text: "Attendance" },
+        { to: "/marks", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772326/Marks_ynnahe.png", text: "Marks" },
+        { to: "/staff", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772308/Staff_ukgko1.png", text: "Staff" },
+        { to: "/leaves", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772269/Leaves_qno92t.png", text: "Leaves" },
+        { to: "/announcements", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772198/Announcements_d7rrb4.png", text: "Announcements" },
+        { to: "/settings", imgSrc: "https://res.cloudinary.com/devewerw3/image/upload/v1720772183/Settings_boymis.png", text: "Settings" },
+      ].map((link, index) => (
+        <Link
+          key={index}
+          to={link.to}
+          className="flex flex-col items-center justify-center p-4 transition-transform duration-300 ease-in-out transform hover:scale-105 bg-white border border-gray-200 rounded-3xl w-10/12 shadow-md hover:shadow-xl"
+          ref={(el) => linksRef.current[index] = el}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={() => handleMouseLeave(index)}
+        >
+          <img src={link.imgSrc} className='w-24' alt="" />
+          <span className="mt-2 text-sm md:text-lg lg:text-lg xl:text-lg font-semibold">{link.text}</span>
+        </Link>
+      ))}
     </>
   )
 }
 
-export default Dbmain
+export default Dbmain;
